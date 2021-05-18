dbe0cbde94ba9a65b94bf9eb212a928b9cbb3683
MACU-Interviews <ehoward@macu.com>
2021-05-18 15:22:26 -0600

    Update cors domain

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors('localhost:8080'));
+app.use(cors('localhost'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
