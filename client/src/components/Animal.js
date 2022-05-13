3bad1143880e0fc79536142efcd8d4c9d0c987fb
Wesley <wesleyneal346@gmail.com>
2022-05-13 09:07:00 -0600

    change the lables

--- client/src/components/Animal.js
+++ client/src/components/Animal.js
@@ -7,15 +7,15 @@ const Animal = ({ animal, bgOption }) => {
                 <div className='column'>
                     <div className='ui equal width grid'>
                         <div className='column'>
-                            <h3>{animal.name}</h3>
+                            <h2>{animal.name}</h2>
                         </div>
                         <div className='column'>
-                            <h4>{animal.age}</h4>
+                            <p>{animal.age}</p>
                         </div>
                     </div>
                 </div>
                 <div className='column'>
-                    <h4>{animal.species}</h4>
+                    <p>{animal.species}</p>
                 </div>
             </div>
         </div>
