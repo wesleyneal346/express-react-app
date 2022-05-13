805e1fbe125d6944828e015afec3ad71e95052c9
Wesley <wesleyneal346@gmail.com>
2022-05-13 02:01:12 -0600

    add doxygen comments to the toy-problems

--- toy-problems/index.js
+++ toy-problems/index.js
@@ -1,5 +1,10 @@
 const data = require("../server/data");
 
+/**
+ * Test the input number for primeness. Must be positive, and not 0 or 1.
+ * @param {number} input an integer to test if it is prime
+ * @returns True if the number is prime, false if otherwise.
+ */
 const isPrime = function (input) {
     // your code here:  this function accepts a positive integer 'input' and should return true if the integer is prime and false if it is not (for simplicity, do not consider 0 or 1 as prime)
 
@@ -33,8 +38,11 @@ const isPrime = function (input) {
     return true;
 };
 
+/**
+ * Count the number of animals that have names with more than three vowels
+ * @returns a total number of animals that have a name with more than three vowels
+ */
 const nameVowels = function () {
-    // your code here:  using 'data', this function should return the number of animals who's name contains more than 3 vowels (for simplicity, do not consider "y" a vowel)
     const vowels = /[aeiou]/gi
     return data.filter(animal => {
         const matchResult = animal.name.match(vowels)
@@ -55,8 +63,12 @@ const addProperty = function () {
     })
 };
 
+/**
+ * Tests whether the input string is a palindrome
+ * @param {string} input The string to test palindromeness
+ * @returns True if the input string is a palindrome, false otherwise
+ */
 const palindromes = function (input) {
-    // your code here:  this function accepts one string 'input' and should return true if the string is a palindrome and false if it is not
     const reversed = [...input].reverse().join("");
     if (reversed.toLowerCase() === input.toLowerCase() ) {
         return true;
@@ -64,8 +76,13 @@ const palindromes = function (input) {
     return false;
 };
 
+/**
+ * Test whether the input strings are anagrams of one another.
+ * @param {string} input1 The first string to test with
+ * @param {string} input2 The second string to test with
+ * @returns true if the two input strings are palindroms of one another, false otherwise
+ */
 const anagrams = function (input1, input2) {
-    // your code here:  this function accepts two strings 'input1', 'input2' and should return true if the strings are anagrams and false if they are not
     temp1 = [...input1].sort();
     temp2 = [...input2].sort();
     
@@ -76,8 +93,11 @@ const anagrams = function (input1, input2) {
     }
 };
 
+/**
+ * Calculates the total age of the animals in the list
+ * @returns a number indicating the total age of the animals in the list 
+ */
 const ageSum = function () {
-    // your code here:  using 'data', this function should return the sum of all animals ages
     let initialValue = 0
     return data.reduce( (previousValue, currentValue) => {
         return previousValue + currentValue.age
